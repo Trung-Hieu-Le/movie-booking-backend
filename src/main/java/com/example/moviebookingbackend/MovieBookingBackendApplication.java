8d1c8e8e7b50a2db6bd8d982224ebb8a682e5662
Trung-Hieu-Le <letrunghieuconj@gmail.com>
2024-04-17 20:57:38 +0700

    stuff

--- src/main/java/com/example/moviebookingbackend/MovieBookingBackendApplication.java
+++ src/main/java/com/example/moviebookingbackend/MovieBookingBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class uMovieBookingBackendApplication {
+public class MovieBookingBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieBookingBackendApplication.class, args);
